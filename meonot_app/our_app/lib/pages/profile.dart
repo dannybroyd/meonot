93053440040e60e4086e848bcf3e6f3128c613e0
doronaloni25 <doronaloni@mail.tau.ac.il>
2023-11-14 14:22:05 +0200

    form update
    made form and text form util

--- meonot_app/our_app/lib/pages/profile.dart
+++ meonot_app/our_app/lib/pages/profile.dart
@@ -1,15 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:our_app/util/form_field.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
-  State<ProfilePage> createState() => _ProfilePageState();
+  State<ProfilePage> createState() => _ProfileFormState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
+class _ProfileFormState extends State<ProfilePage> {
+final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    return Scaffold(
+      body: Padding(padding:  const EdgeInsets.all(30),
+       child: Form(
+        key: _formKey,
+        child: ListView(
+        children: [
+          MainFormField(
+            hintText: 'Name',
+            validator:(val){
+              if(val != null) {
+                return "ENTER VALID NAME";
+              }
+              return null;
+              }),
+
+          MainFormField(
+            hintText: 'Phone',
+            validator:(val){
+              if(val != null) {
+                return "ENTER VALID PHONE";
+              }
+              return null;
+              }),
+          MainFormField(
+            hintText: 'Meonot',
+            validator:(val){
+              if(val != null) {
+                return "ENTER VALID MEONOT";
+              }
+              return null;
+              }),
+          MainFormField(
+            hintText: 'Building',
+            validator:(val){
+              return null;
+              }),
+          MainFormField(
+            hintText: 'Floor',
+            validator:(val){
+              return null;
+              }),
+          MainFormField(
+            hintText: 'Appartment',
+            validator:(val){
+              return null;
+              }),
+           const SizedBox(height: 30,),
+          ElevatedButton(onPressed: (){} ,style: ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)))), child: const Text("Submit")), 
+        ],
+       )),
+      ),
+    );
   }
 }
---
+++ meonot_app/our_app/lib/util/form_field.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class MainFormField extends StatelessWidget {
+  const MainFormField({
+    super.key,
+    required this.hintText,
+    this.validator,
+
+    });
+
+  final String hintText;
+
+  final String? Function(String?)? validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: TextFormField(
+        validator: validator,
+        decoration: InputDecoration(
+          hintText: hintText,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+
+      ),
+    );
+  }
+}
+
