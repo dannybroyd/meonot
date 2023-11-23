322ca305333c14090d34ad4c0505504accfe26d5
doronaloni25 <doronaloni@mail.tau.ac.il>
2023-11-23 16:31:36 +0200

    added text box for maintenance

--- meonot_app/our_app/lib/pages/maintenance.dart
+++ meonot_app/our_app/lib/pages/maintenance.dart
@@ -10,6 +10,29 @@ class MaintenancePage extends StatefulWidget {
 class _MaintenancePageState extends State<MaintenancePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: const MyAppBar(text: "פנייה בנושא תחזוקה ", middle: true,),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            const Center(
+              child: Text(" :הפנייה שלי",style: TextStyle(fontSize: 26 ),)
+              ),
+            FormBuilder(
+              child: Container(
+                width: 50, height: 800,
+                padding: const EdgeInsets.all(16.0),
+                child: FormBuilderTextField(
+                  name: "maintenance",
+                  decoration: const InputDecoration(border: OutlineInputBorder(),contentPadding: EdgeInsets.all(8),),
+                  style: TextStyle(fontSize: 25),maxLines: 12, minLines: 8,
+                  ),
+              )
+              )
+            ],
+        ),
+      )
+    );
   }
 }
